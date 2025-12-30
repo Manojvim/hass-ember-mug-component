5dbe11e75b8fedd8bd5b91eedda7c84ebf7d2cb9
Manoj <manoj.vim@gmail.com>
2025-12-30 23:46:29 +0100

    fix: use standard type alias for better compatibility

--- custom_components/ember_mug/__init__.py
+++ custom_components/ember_mug/__init__.py
@@ -32,7 +32,7 @@ if TYPE_CHECKING:
     from homeassistant.core import Event, HomeAssistant
 
 
-type EmberMugConfigEntry = ConfigEntry[MugDataUpdateCoordinator]
+EmberMugConfigEntry = ConfigEntry[MugDataUpdateCoordinator]
 
 
 PLATFORMS = [
